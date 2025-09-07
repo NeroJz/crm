8db876c57808d657dcef60e2db7135b8aae7fa4f
KumHoe <nerojz86@gmail.com>
2025-09-07 14:26:19 +0800

    [user] Added create user route and logic

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,34 @@
-export class User {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, Unique, UpdateDateColumn } from 'typeorm';
+
+export enum UserRole {
+  NONE = 'none',
+  ADMIN = 'admin',
+  SALES = 'sales'
+}
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({
+    unique: true
+  })
+  email: string;
+
+  @Column({
+    type: 'enum',
+    enum: UserRole,
+    default: UserRole.NONE
+  })
+  role: string;
+
+  @CreateDateColumn()
+  createdAt: string;
+
+  @UpdateDateColumn()
+  updatedAt: string;
+}
