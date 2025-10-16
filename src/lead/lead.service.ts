ccc26e693937498d614cb3b3d72d87ba0f727bf1
KumHoe <nerojz86@gmail.com>
2025-10-16 21:24:49 +0800

    [lead] Add convert-to-opportunity handling

--- src/lead/lead.service.ts
+++ src/lead/lead.service.ts
@@ -2,9 +2,11 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateLeadDto } from './dto/create-lead.dto';
 import { UpdateLeadDto } from './dto/update-lead.dto';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Lead } from './entities/lead.entity';
+import { Lead, LeadType } from './entities/lead.entity';
 import { Repository } from 'typeorm';
 import { User } from 'src/users/entities/user.entity';
+import { Pipeline } from 'src/pipelines/entities/pipeline.entity';
+import { ConvertOpportunityDto } from './dto/convert-opportunity.dto';
 
 @Injectable()
 export class LeadService {
@@ -13,7 +15,10 @@ export class LeadService {
     private readonly leadRepository: Repository<Lead>,
 
     @InjectRepository(User)
-    private readonly userRespository: Repository<User>
+    private readonly userRespository: Repository<User>,
+
+    @InjectRepository(Pipeline)
+    private readonly pipelineRepository: Repository<Pipeline>
   ) { }
 
   async create(createLeadDto: CreateLeadDto, user: User) {
@@ -22,6 +27,18 @@ export class LeadService {
         ...createLeadDto,
         owner: user
       });
+
+    const { pipelineId } = createLeadDto;
+    if (pipelineId) {
+      const pipeline = await this.pipelineRepository
+        .findOneBy({ id: pipelineId });
+
+      if (!pipeline) {
+        throw new NotFoundException('Pipeline not found. Unable assign the lead to the pipeline.');
+      }
+
+      lead.pipeline = pipeline;
+    }
 
     await this.leadRepository.save(lead);
 
@@ -70,6 +87,34 @@ export class LeadService {
     }
 
     Object.assign(lead, updateLeadDto);
+
+    return this.leadRepository.save(lead);
+  }
+
+  async updateToOpportunity(id: string, convertOpportunityDto: ConvertOpportunityDto) {
+    let lead = await this.leadRepository
+      .findOneBy({ id });
+
+    if (!lead) {
+      throw new NotFoundException('Lead not found.');
+    }
+
+    let pipeline = await this.pipelineRepository
+      .findOneBy({ id: convertOpportunityDto.pipelineId });
+
+    if (!pipeline) {
+      throw new NotFoundException('Pipeline not found. Unable to update the lead to opportunity.');
+    }
+
+    if (!lead.pipeline) {
+      lead.pipeline = pipeline;
+    }
+
+    if (lead.pipeline.id !== pipeline.id) {
+      throw new NotFoundException('The pipeline id is unmatched with the current lead. Unable to convert to opportunity.');
+    }
+
+    lead.type = LeadType.Opportunity;
 
     return this.leadRepository.save(lead);
   }
